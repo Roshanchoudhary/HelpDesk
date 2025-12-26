2fd5b44339f51c64eec76e9951924850a434588a
Roshanchoudhary <roshanchoudhary@hotmail.com>
2025-12-26 20:38:50 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views  # Naya import
 
 urlpatterns = [
+    path('', views.set_password, name='set_password'), # Home page par ye chalega
     path('admin/', admin.site.urls),
 ]
